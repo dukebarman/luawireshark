ddfbd727bb55eb7d05dd5a38788f9e4bdb015388
dukeBarman <dukebarman@gmail.com>
2013-11-11 01:12:39 +0400

    First release
    Lua script for listing host and cookies in Wireshark

---
+++ list_cookies_simple.lua
@@ -0,0 +1,25 @@
+cookiewindow = TextWindow.new("List cookies")
+
+do
+  local hostname = Field.new("http.host")
+  local cookiedata = Field.new("http.cookie")
+
+  local function init_listener()
+    local tap = Listener.new("http")
+    function tap.packet(pinfo,buffer,userdata)
+      local targethost = hostname()
+      local targetcookie = cookiedata()
+
+      if targethost ~= nil then
+        if targethost ~= nil then
+          cookiewindow:append(tostring(targethost))
+          cookiewindow:append("\n")
+          cookiewindow:append(tostring(targethost))
+          cookiewindow:append("\n\n")
+        end
+      end
+    end
+  end
+
+  init_listener()
+end
